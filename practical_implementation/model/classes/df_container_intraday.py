dbe911d61da0991ee3354c9b677a868ac5ba506e
Marat Galiulin <egomarat@gmail.com>
2023-12-14 01:14:21 +1100

    подчистил файл start.ipynb

--- practical_implementation/model/classes/df_container_intraday.py
+++ practical_implementation/model/classes/df_container_intraday.py
@@ -9,8 +9,8 @@ class dfContainerIntraDay:
     def __init__(self, df_path):
         self.df_path = df_path
         self.curr_df = pd.read_csv(self.df_path, sep=';', dtype=str)
-        self.curr_df.columns = ['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Vol']
-        self.curr_df[['Open', 'High', 'Low', 'Close', 'Vol']] = self.curr_df[['Open', 'High', 'Low', 'Close', 'Vol']].astype('float32')
+        self.curr_df.columns = ['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+        self.curr_df[['Open', 'High', 'Low', 'Close', 'Volume']] = self.curr_df[['Open', 'High', 'Low', 'Close', 'Volume']].astype('float32')
 
         self.curr_df.index = self.curr_df.apply(date_time_index_maker, axis=1)
 
@@ -23,11 +23,11 @@ class dfContainerIntraDay:
     def show_head(self, num:int):
         print(self.curr_df.head(num))
 
-    def draw_graph(self, end_date:str, start_date='', fig_sz = (8,8)):
+    def draw_graph(self, end_date:str, start_date='', fig_sz = (8,8), vol=False):
         if(start_date != ''):
-            mpf.plot(self.curr_df.loc[pd.to_datetime(start_date):pd.to_datetime(end_date)], type='candle',mav=(3,6,9), figsize=fig_sz)
+            mpf.plot(self.curr_df.loc[pd.to_datetime(start_date):pd.to_datetime(end_date)], type='candle',mav=(3,6,9), figsize=fig_sz, volume=vol)
             plt.show()
 
         else:
-            mpf.plot(self.curr_df.loc[:pd.to_datetime(end_date)], type='candle',mav=(3,6,9), figsize=fig_sz)
+            mpf.plot(self.curr_df.loc[:pd.to_datetime(end_date)], type='candle',mav=(3,6,9), figsize=fig_sz, volume=vol)
             plt.show()
