e0c59294163cf11339ed1114e11f042b1d54b21e
Marat Galiulin <egomarat@gmail.com>
2023-12-23 16:16:36 +1100

    добавил вычисление и отрисовку зон ликвидности, подсчет объема для каждой зоны ликвидности

--- practical_implementation/model/classes/draw_date_offset_graph.py
+++ practical_implementation/model/classes/draw_date_offset_graph.py
@@ -13,10 +13,21 @@ def draw_do(df:pd.DataFrame, d_o: pd.DateOffset(), start_date, fig_sz, vol, type
             DCM = df[['Middle']].loc[stdt:end_date]
             DCL = df[['Lower']].loc[stdt:end_date]
             Plot_RSI = df['rsi'].loc[stdt:end_date]
+            Daily_High_Limit = df['daily_high'].loc[stdt:end_date]
+            Daily_Low_Limit = df['daily_low'].loc[stdt:end_date]
+            Upper_Zone_Limit = df['upper_zone_limit'].loc[stdt:end_date]
+            Lower_Zone_Limit = df['lower_zone_limit'].loc[stdt:end_date]
             apds = [
                 mpf.make_addplot(DCU,color='#2962FF',panel=0),
                 mpf.make_addplot(DCM,color='#FF6D00',panel=0),
                 mpf.make_addplot(DCL,color='#2962FF',panel=0),
+
+                mpf.make_addplot(Daily_High_Limit,color='#00aa14',panel=0),
+                mpf.make_addplot(Daily_Low_Limit,color='#b60431',panel=0),
+
+                mpf.make_addplot(Upper_Zone_Limit,color='#00aa14',panel=0),
+                mpf.make_addplot(Lower_Zone_Limit,color='#b60431',panel=0),
+
                 mpf.make_addplot(Plot_RSI,panel=2,color='lime',ylim=(10,90),secondary_y=True),
             ]
             mpf.plot(df.loc[stdt:end_date], type=type, figsize=fig_sz, volume=vol, style=binance_dark, addplot=apds) # отрисовывает график
@@ -29,10 +40,21 @@ def draw_do(df:pd.DataFrame, d_o: pd.DateOffset(), start_date, fig_sz, vol, type
             DCM = df[['Middle']].loc[stdt:end_date]
             DCL = df[['Lower']].loc[stdt:end_date]
             Plot_RSI = df['rsi'].loc[stdt:end_date]
+            Daily_High_Limit = df['daily_high'].loc[stdt:end_date]
+            Daily_Low_Limit = df['daily_low'].loc[stdt:end_date]
+            Upper_Zone_Limit = df['upper_zone_limit'].loc[stdt:end_date]
+            Lower_Zone_Limit = df['lower_zone_limit'].loc[stdt:end_date]
             apds = [
                 mpf.make_addplot(DCU,color='#2962FF',panel=0),
                 mpf.make_addplot(DCM,color='#FF6D00',panel=0),
                 mpf.make_addplot(DCL,color='#2962FF',panel=0),
+
+                mpf.make_addplot(Daily_High_Limit,color='#00aa14',panel=0),
+                mpf.make_addplot(Daily_Low_Limit,color='#b60431',panel=0),
+
+                mpf.make_addplot(Upper_Zone_Limit,color='#00aa14',panel=0),
+                mpf.make_addplot(Lower_Zone_Limit,color='#b60431',panel=0),
+
                 mpf.make_addplot(Plot_RSI,panel=2,color='lime',ylim=(10,90),secondary_y=True),
             ]
             mpf.plot(df.loc[stdt:end_date], type=type, figsize=fig_sz, volume=vol, style=binance_dark, addplot=apds) # отрисовывает график
